1b725516efc45f9dc39cbcc52645a8e49babe23f
Leandro de los Santos <leandrodls@gmail.com>
2022-10-10 14:10:18 -0300

    23-24

--- Practica_Angular/ejercicio23/src/app/app.module.ts
+++ Practica_Angular/ejercicio23/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,9 +16,10 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, ReactiveFormsModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- Practica_Angular/ejercicio23/src/app/create-event/create-event.component.ts
+++ Practica_Angular/ejercicio23/src/app/create-event/create-event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventService } from '../event.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
@@ -9,12 +10,29 @@ import { EventService } from '../event.service';
 })
 export class CreateEventComponent implements OnInit {
 
-  constructor(private eventService: EventService, private router: Router) { }
+  constructor(private ef: FormBuilder, private eventService: EventService, private router: Router) { }
 
-  ngOnInit() {
+  eventForm = this.ef.group({
+  name : [''],
+  date : [''] ,
+  time : [''] ,
+  location : this.ef.group({
+    address : [''],
+    city : [''],
+    country : [''],
+    })
+  }); 
+  
+  ngOnInit(): void {
+   
   }
 
-  cancel() {
+  cancel(): void {
     this.router.navigate(['/events']);
+  }
+
+  onSubmit(){
+    this.eventService.saveEvent(this.eventForm.value);
+    this.router.navigate(["/events"]);
   }
 }
